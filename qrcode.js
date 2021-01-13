c3f801adf3d519415b97fc0458fd45f94d1bc437
LaurentJeannel <jeannellaurent11@gmail.com>
2021-01-13 20:11:54 +0100

    news
    news

---
+++ qrcode.js
@@ -0,0 +1,33 @@
+exports.action = function(data){
+
+var path	= require	('path')
+//var request = require('request');
+var spawn = require('child_process').spawn;
+
+var ahk = spawn(path.resolve('%CD%', './plugins/qrcode/zbar/bin/zbarcam.exe').replace('\\%CD%', ''));
+
+ahk.stdout.on('data', function(dataQRCode){
+	dataQRCode=dataQRCode.toString() ; dataQRCode=dataQRCode.replace('QR-Code:','')
+	
+	console.log(dataQRCode)
+TraitementQRCode(dataQRCode)
+
+})
+
+
+var TraitementQRCode=function(dataQRCode){
+
+	if(dataQRCode.search('http')>-1){console.log('url')
+//go to http....
+}
+		
+}
+
+
+
+
+
+
+
+
+}
